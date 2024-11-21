e8379fed45c470c64f478be7e8ff179930d6e801
MrJackSpade <loffredo.ap@gmail.com>
2024-11-21 15:36:50 -0800

    no message

--- payload/mnt/mmc/Roms/APPS/XFCE.sh
+++ payload/mnt/mmc/Roms/APPS/XFCE.sh
@@ -3,5 +3,5 @@ progdir=`dirname "$0"`
 log=$progdir/XFCE.log
 rm /var/log/Xorg*
 export QT_X11_NO_MITSHM=1
-ntpdate time.nist.gov
+date -us "$(wget -qS --spider google.com 2>&1 | grep "^  Date:" | sed 's/  Date: //' | head -n1)"
 startx > $log 2>&1
